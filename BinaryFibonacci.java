5278b4b8b1b64f7876e2206ec2ad11ae62e14569
Younesbhb <122587727+Younesbhb@users.noreply.github.com>
2023-05-17 00:05:35 -0400

    Add files via upload

---
+++ BinaryFibonacci.java
@@ -0,0 +1,58 @@
+/**
+ * Course : COMP 352
+ * Student ID : 40205816
+ * @author Younes Bouhaba
+ *
+ */
+import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+public class BinaryFibonacci {
+    /**
+     Algorithm BinaryFibonacci(n)
+     Input: Nonnegative integer n
+     Output: The nth Fibonacci number
+     if n = 0 then
+     return 0
+     if n = 1 then
+     return 1
+     else then
+     return BinaryFibonacci(n - 1) + BinaryFibonacci(n - 2)
+     */
+
+    public static long BinaryFibonacci (long n)
+    {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        else {
+            return BinaryFibonacci(n - 1) + BinaryFibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        try {
+            PrintStream text = new PrintStream(new FileOutputStream("BinaryFibonacci.txt"));
+            System.setOut(text);
+        } catch (FileNotFoundException e) {
+            System.out.println("There is a problem and BinaryFibonacci.txt was not created");
+        }
+
+        for (int i = 0; i <= 45; i = i + 5) {
+            long startTime = System.nanoTime();
+            long FibNumber = BinaryFibonacci(i);
+            long endTime = System.nanoTime();
+            long totalTime = endTime - startTime;
+            System.out.println("The " + i + "th Fibonacci number is: " +  FibNumber + "\nThe total time is : " + totalTime + " nanoseconds");
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ LinearFibonacci.java
@@ -0,0 +1,55 @@
+/**
+ * Course : COMP 352
+ * Student ID : 40205816
+ * @author Younes Bouhaba
+ *
+ */
+import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+public class LinearFibonacci {
+    /**
+     Algorithm LinearFibonacci(n):
+     Input: A nonnegative integer n
+     Output: Pair of Fibonacci numbers Fn-1,Fn-2
+     if n = 1 then
+     return (n, 0)
+     else then
+     (i, j) = LinearFibonacci(n − 1)
+     return (i +j, i)
+     */
+
+    public static long[] LinearFibonacci(long n)
+    {
+        if (n == 1) {
+            return new long [] {n, 0};
+        }
+        else {
+            long [] FibNumber = LinearFibonacci(n - 1);
+            return new long [] {FibNumber[0] + FibNumber[1], FibNumber[0]};
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            PrintStream text = new PrintStream(new FileOutputStream("LinearFibonacci.txt"));
+            System.setOut(text);
+        } catch (FileNotFoundException e) {
+            System.out.println("There is a problem and LinearFibonacci.txt was not created");
+        }
+
+        for (int i = 5; i <= 100; i += 5) {
+            long startTime = System.nanoTime();
+            long [] FibNumbers = LinearFibonacci(i);
+            long endTime = System.nanoTime();
+            long totalTime = endTime - startTime;
+
+            System.out.println("The " + i + "th Fibonacci number is: " +  FibNumbers[0] + "\n The total time is : " + totalTime + " nanoseconds");
+            System.out.println();
+        }
+
+    }
+
+}
